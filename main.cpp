823ecaef0dffb025212fcbf4aa3e21fb9f1e77e6
sp1rale <dimayaremchyk24@gmail.com>
2023-10-22 15:26:18 +0300

    done

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main() {
-	cout << "home work";
+	int m;
+	cin >> m;
+
+	if (m < 0)
+	cout << "positiv";
+
+	else if (m > 0)
+	cout << "negativ";
+
+	else
+		cout << "zero";
 	return 0;
 }
