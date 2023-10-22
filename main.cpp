522e707169e79cf13cba396a999686e5806f4f81
sp1rale <dimayaremchyk24@gmail.com>
2023-10-22 15:01:21 +0300

    done

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 int main() {
-	cout << "home work";
+	int f, k;
+
+	cin >> f >> k;
+	
+	if (f < k)
+	cout << "first is less";
+
+	else if (f > k)
+	cout << "second is less";
+
+	else
+	cout << "equal";
 	return 0;
 }
